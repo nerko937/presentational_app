e6b88e432aef6eb3e1f4fa93743eee26aab42dd7
Marcin Fudala <nerko937@gmail.com>
2020-07-11 11:23:51 +0200

    improved footer, added responsivness to it, changed layout to be based on flexbox

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,30 @@
 <template>
   <div id="app">
-
-    <footer id="footer">
-      <div class="footer-column">
-        <span class="footer-row">created by:</span><br/>
-        <span class="footer-row">Marcin Fudala</span><br/>
-        <span class="footer-row">nerko937@gmail.com</span><br/>
-        <span class="footer-row"><a href="https://github.com/nerko937">Github</a></span>
+    <Footer/>
       </div>
-    </footer>
-  </div>
 </template>
 
 <script>
+import Footer from '@/components/Footer.vue'
 export default {
   name: 'App',
   components: {
-
+    Main, Footer
   }
 }
 </script>
 
 <style lang="sass">
-html, body, #app, #footer-column
+html, body, #app
   height: 100%
 
 body
   background-image: url('./assets/background.jpg')
   margin: 0
+
+#app
+  display: flex
+  flex-direction: column
 
 a
   text-decoration: none
@@ -36,25 +33,4 @@ a
 a:hover
   text-decoration: underline
 
-#app
-  display: flex
-  width: 100%
-
-footer
-  align-self: flex-end
-  display: flex
-  justify-content: flex-end
-  width: 100%
-  height: 25%
-  background-color: $background-color
-
-.footer-column
-  width: 15%
-  margin: auto 6rem
-
-.footer-row
-  line-height: 1.8rem
-  font-size: small
-  color: $secondary-color
-  
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,75 @@
+<template>
+  <footer class="footer">
+    <div class="footer-column example-col">
+      <div class="footer-row" v-for="n in 4" :key="n">
+        <span class="footer-row"><a href="#">Link {{ n }}</a></span><br/>
+      </div>
+    </div>
+    <div class="footer-divider"/>
+    <div class="footer-column example-col">
+      <div class="footer-row" v-for="n in 4" :key="n">
+        <span class="footer-row"><a href="#">Link {{ n + 4 }}</a></span><br/>
+      </div>
+    </div>
+    <div class="footer-divider"/>
+    <div class="footer-column">
+      <span class="footer-row">created by:</span><br/>
+      <span class="footer-row">Marcin Fudala</span><br/>
+      <span class="footer-row">nerko937@gmail.com</span><br/>
+      <span class="footer-row"><a href="https://github.com/nerko937">Github</a></span>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="sass" scoped>
+
+.footer
+  +center
+  flex-shrink: 0
+  background-color: $background-color
+
+.footer-row
+  line-height: 1.8rem
+  font-size: small
+  color: $secondary-color
+  font-family: Times, "Times New Roman", serif
+
+@media (max-width: $breakpoint-phone)
+
+  .footer
+    flex-direction: column
+
+  .footer-column
+    width: 60%
+    padding-top: 0.5rem
+    padding-bottom: 0.5rem
+
+  .footer-divider
+    height: 1px
+    background-color: #616161
+    width: 80%
+
+@media (min-width: $breakpoint-phone)
+
+  .footer
+    height: 25%
+
+  .footer-column
+    width: 8rem
+    margin: auto 4rem
+
+  .footer-divider
+    width: 1px
+    background-color: #616161
+    height: 80%
+
+  .example-col
+    text-align: center
+
+</style>
