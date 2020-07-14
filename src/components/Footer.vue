226d92e70a70ae979a5023ea1c20ddf5d0b35e03
Marcin Fudala <nerko937@gmail.com>
2020-07-14 19:30:25 +0200

    added bem namings

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,22 @@
 <template>
   <footer class="footer">
-    <div class="footer-column example-col">
-      <div class="footer-row" v-for="n in 4" :key="n">
-        <span class="footer-row"><a href="#">Link {{ n }}</a></span><br/>
+    <div class="footer__column example-col">
+      <div class="footer__row" v-for="n in 4" :key="n">
+        <span class="footer__row"><a href="#">Link {{ n }}</a></span><br/>
       </div>
     </div>
-    <div class="footer-divider"/>
-    <div class="footer-column example-col">
-      <div class="footer-row" v-for="n in 4" :key="n">
-        <span class="footer-row"><a href="#">Link {{ n + 4 }}</a></span><br/>
+    <div class="footer__divider"/>
+    <div class="footer__column example-col">
+      <div class="footer__row" v-for="n in 4" :key="n">
+        <span class="footer__row"><a href="#">Link {{ n + 4 }}</a></span><br/>
       </div>
     </div>
-    <div class="footer-divider"/>
-    <div class="footer-column">
-      <span class="footer-row">created by:</span><br/>
-      <span class="footer-row">Marcin Fudala</span><br/>
-      <span class="footer-row">nerko937@gmail.com</span><br/>
-      <span class="footer-row"><a href="https://github.com/nerko937">Github</a></span>
+    <div class="footer__divider"/>
+    <div class="footer__column">
+      <span class="footer__row">created by:</span><br/>
+      <span class="footer__row">Marcin Fudala</span><br/>
+      <span class="footer__row">nerko937@gmail.com</span><br/>
+      <span class="footer__row"><a href="https://github.com/nerko937">Github</a></span>
     </div>
   </footer>
 </template>
@@ -28,48 +28,46 @@
 </script>
 
 <style lang="sass" scoped>
-
 .footer
   +center
   flex-shrink: 0
   background-color: $color-background
 
-.footer-row
-  line-height: 1.8rem
-  font-size: small
-  color: $color-primary
-  font-family: Times, "Times New Roman", serif
-
-@media (max-width: $breakpoint-phone)
-
-  .footer
+  @media (max-width: $breakpoint-phone)
     flex-direction: column
 
-  .footer-column
-    width: 60%
-    padding-top: 0.5rem
-    padding-bottom: 0.5rem
-
-  .footer-divider
-    height: 1px
-    background-color: #616161
-    width: 80%
-
-@media (min-width: $breakpoint-phone)
-
-  .footer
+  @media (min-width: $breakpoint-phone)
     height: 25%
 
-  .footer-column
-    width: 8rem
-    margin: auto 4rem
+  &__column
+    @media (max-width: $breakpoint-phone)
+      width: 60%
+      padding-top: 0.5rem
+      padding-bottom: 0.5rem
 
-  .footer-divider
-    width: 1px
-    background-color: #616161
-    height: 80%
+    @media (min-width: $breakpoint-phone)
+      width: 8rem
+      margin: auto 4rem
 
-  .example-col
+  &__row
+    line-height: 1.8rem
+    font-size: small
+    color: $color-primary
+    font-family: Times, "Times New Roman", serif
+  
+  &__divider
+    @media (max-width: $breakpoint-phone)
+      height: 1px
+      background-color: #616161
+      width: 80%
+
+    @media (min-width: $breakpoint-phone)
+      width: 1px
+      background-color: #616161
+      height: 80%
+
+.example-col
+  @media (min-width: $breakpoint-phone)
     text-align: center
 
 </style>
