6e32364644b5ff311e0a83e71bfa0e25681bf839
Marcin Fudala <nerko937@gmail.com>
2020-07-19 11:24:52 +0200

    added temporary lorem ipsum home tab

--- src/components/main/Home.vue
+++ src/components/main/Home.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-
+  <div class="container">
+    <p class="container__text">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec pulvinar lorem. Integer pulvinar dui eget bibendum tristique. Morbi pulvinar ultricies augue, eget dictum ipsum interdum eget. Interdum et malesuada fames ac ante ipsum primis in faucibus. In vel odio tempor, laoreet neque eu, pellentesque massa. Suspendisse viverra risus non urna iaculis, at finibus tortor varius. Pellentesque mollis hendrerit pulvinar. 
+    </p>
   </div>
 </template>
 
@@ -11,5 +13,9 @@
 </script>
 
 <style lang="sass" scoped>
+.container
+  padding: 1rem 2rem
 
+  &__text
+    color: $color-primary
 </style>
