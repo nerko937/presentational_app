b65bc8e0ed8a56b76be5c8e75bcd41ad3693f475
Marcin Fudala <nerko937@gmail.com>
2020-10-12 17:32:39 +0200

    added carousel to homepage

--- src/components/main/Home.vue
+++ src/components/main/Home.vue
@@ -1,21 +1,77 @@
 <template>
-  <div class="container">
-    <p class="container__text">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec pulvinar lorem. Integer pulvinar dui eget bibendum tristique. Morbi pulvinar ultricies augue, eget dictum ipsum interdum eget. Interdum et malesuada fames ac ante ipsum primis in faucibus. In vel odio tempor, laoreet neque eu, pellentesque massa. Suspendisse viverra risus non urna iaculis, at finibus tortor varius. Pellentesque mollis hendrerit pulvinar. 
-    </p>
-  </div>
+  <agile autoplay :autoplaySpeed='6000' id="carousel">
+    <div
+      v-for="(slide, i) in slides"
+      :key="i"
+      class="slide"
+      :style="`background-image: url(${slide.url})`"
+    >
+      <div class="slide__title">
+        <h2>{{ slide.title }}</h2>
+      </div>
+      <div class="slide__text">
+        <p>{{ slide.text }}</p>
+      </div>
+    </div>
+
+    <template slot="prevButton">
+      <font-awesome-icon icon="chevron-left"/>
+    </template>
+    <template slot="nextButton">
+      <font-awesome-icon icon="chevron-right"/>
+    </template>
+  </agile>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {
+  data() {
+    return {
+      slides: [
+        {
+          title: 'Title1',
+          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan tempor orci sit amet pretium.',
+          url: require('@/assets/slide1.jpg'),
+        },
+        {
+          title: 'Title1',
+          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan tempor orci sit amet pretium.',
+          url: require('@/assets/slide2.jpg'),
+        },
+        {
+          title: 'Title1',
+          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan tempor orci sit amet pretium.',
+          url: require('@/assets/slide3.jpg'),
+        }
+      ]
+    }
+  },
+};
 </script>
 
 <style lang="sass" scoped>
-.container
-  padding: 1rem 2rem
+#carousel
+  margin: 1rem 2rem
+
+.slide
+  height: 35rem
+  background-size: cover
+  &__title
+    margin-top: 3rem
+    color: $color-primary
+    text-align: center
 
   &__text
     color: $color-primary
+    height: 70%
+    display: flex
+    justify-content: center
+    align-items: flex-end
+  
+    @media (min-width: $breakpoint-phone)
+      margin: 0 2rem
+  
+    @media (max-width: $breakpoint-phone)
+      padding: 0 1rem
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueAgile from 'vue-agile'
+import "@/sass/carousel.sass"
+
+Vue.use(VueAgile)
+
 Vue.config.productionTip = false
 
 new Vue({
