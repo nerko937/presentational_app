e30e710cfe9f9d23329a56d3897424388ce89e8e
Marcin Fudala <nerko937@gmail.com>
2020-10-12 17:36:03 +0200

    added font awesome

--- src/main.js
+++ src/main.js
@@ -4,7 +4,14 @@ import App from './App.vue'
 import VueAgile from 'vue-agile'
 import "@/sass/carousel.sass"
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 Vue.use(VueAgile)
+
+library.add(faChevronLeft, faChevronRight)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
