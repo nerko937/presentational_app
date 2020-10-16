8dc17169b0629e92981d46a5166a7f769ca81ed3
Marcin Fudala <nerko937@gmail.com>
2020-10-16 22:26:08 +0200

    github pages deployment

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-    css: {
-      loaderOptions: {
-        sass: {
-          additionalData: `
-            @import "@/sass/_variables.sass"
-            @import "@/sass/_mixins.sass"
-          `
-        }
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/sass/_variables.sass"
+          @import "@/sass/_mixins.sass"
+        `
       }
     }
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/presentational_app/'
+    : '/'
+}
   
